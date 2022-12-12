80c6e57636ea5c0160b90baba4af346c74fcbec5
AlexStorm <info@alexstorm.ru>
2022-12-12 16:32:43 +0530

    fonts

--- build/build.sh
+++ build/build.sh
@@ -7,17 +7,7 @@ build_image(){
   docker buildx build  --no-cache --platform ${ARCHS}  -t  $REG_ADDRESS/$ITEM -f ./build/solenopsys.Dockerfile --push ./
 }
 
-build_helm(){
-    echo "HELM PACKAGE"
-    mkdir tmp
-    res=$(helm package ./build/install -d ./tmp/$ITEM)
 
-    file=${res/Successfully packaged chart and saved it to: /}
-    echo "HELM PUSH $file"
-    curl --data-binary "@$file" http://helm.alexstorm.solenopsys.org/api/charts
-    rm -r tmp
-
-}
 
 #build_image
 build_helm
