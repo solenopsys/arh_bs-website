7106fc4529010262b3b5e93eadf75bac1fcf2052
AlexStorm <info@alexstorm.ru>
2023-03-12 16:57:37 +0530

    change user story

--- src/app/base-layout/base-layout.component.ts
+++ src/app/base-layout/base-layout.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'sc-base-layout',
   templateUrl: './base-layout.component.html',
-  styleUrls: ['./base-layout.component.css'],
+  styleUrls: ['./base-layout.component.scss'],
   encapsulation: ViewEncapsulation.Emulated,
 })
 export class BaseLayoutComponent implements OnInit {
